a06c261ac5fb859898581729a3c06a3a85eaafd1
MrAdib <Mr.Javad.Adib@gmail.com>
2022-05-06 23:44:59 +0430

    replace field in everywhere

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var fieldTitle = $(this).attr('data-fill');
     var fieldValue = $(this).text();
     var msg = "Enter new value for [ " +fieldTitle + " ]"
+    var fieldSelector = "[data-fill='" + fieldTitle + "']";
 
     // get new value from propmt
     var newVal = window.prompt(msg, fieldValue);
@@ -13,7 +14,8 @@ $(document).ready(function(){
       newVal = "[" + fieldTitle + "]";
     }
 
-    $(this).text(newVal);
+    // replace with new text on all part of docs
+    $(fieldSelector).text(newVal);
   });
 
 });
