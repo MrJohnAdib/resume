046a3dc1c6069738afd9309b63df8d27ab9edb1b
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-05-06 20:22:49 +0430

    improve msg

--- script/script.js
+++ script/script.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
   $('[data-fill]').on('click', function(){
     var fieldTitle = $(this).attr('data-fill');
     var fieldValue = $(this).text();
+    var msg = "Enter new value for [ " +fieldTitle + " ]"
 
     // get new value from propmt
-    var newVal = window.prompt(fieldTitle, fieldValue);
+    var newVal = window.prompt(msg, fieldValue);
 
     if(!newVal)
     {
