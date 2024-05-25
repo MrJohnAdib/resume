1dd5aca9dd6faee0e8f9ffa1d5135e43094f72c3
MrAdib <Mr.John.Adib@gmail.com>
2024-05-25 01:09:38 +0100

    update the script and use vanillaJS

--- script/script.js
+++ script/script.js
@@ -1,25 +1,39 @@
-$(document).ready(function () {
-  var myDate = new Date().toLocaleString("en-us", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
+var myDate = new Date().toLocaleString("en-us", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+});
+document.querySelectorAll("[data-fill-current]").forEach((el) => {
+  el.textContent = myDate;
+  el.setAttribute("date", myDate);
+});
+
+
+document.querySelector('#contactBox').addEventListener('click', () => {
+  document.querySelector('#phoneBox').classList.remove('hidden');
+});
+
+function replaceDataOnClick(event) {
+  const element = event.target;
+  const fieldTitle = element.getAttribute('data-fill');
+  const fieldValue = element.textContent;
+  const msg = `Enter new value for [ ${fieldTitle} ]`;
+  const fieldSelector = `[data-fill='${fieldTitle}']`;
+
+  // Get new value from prompt
+  let newVal = window.prompt(msg, fieldValue);
+
+  if (!newVal) {
+    newVal = `[${fieldTitle}]`;
+  }
+
+  // Replace with new text on all parts of the document
+  document.querySelectorAll(fieldSelector).forEach(field => {
+    field.textContent = newVal;
+    field.setAttribute('data-fill-val', newVal);
   });
-  $("[data-fill-current]").text(myDate).attr("date", myDate);
+}
 
-  $("[data-fill]").on("click", function () {
-    var fieldTitle = $(this).attr("data-fill");
-    var fieldValue = $(this).text();
-    var msg = "Enter new value for [ " + fieldTitle + " ]";
-    var fieldSelector = "[data-fill='" + fieldTitle + "']";
-
-    // get new value from propmt
-    var newVal = window.prompt(msg, fieldValue);
-
-    if (!newVal) {
-      newVal = "[" + fieldTitle + "]";
-    }
-
-    // replace with new text on all part of docs
-    $(fieldSelector).text(newVal).attr("data-fill-val", newVal);
-  });
+document.querySelectorAll('[data-fill]').forEach(function (element) {
+  element.addEventListener('click', replaceDataOnClick);
 });
